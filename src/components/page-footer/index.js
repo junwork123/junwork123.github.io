b068203695464c5a761cba3bfff60094a6cf7b5b
MaliciousJ <junwork123@gmail.com>
2022-06-30 23:31:12 +0900

    댓글 utterance 기능 추가

--- src/components/page-footer/index.js
+++ src/components/page-footer/index.js
@@ -12,7 +12,19 @@ function PageFooter({ author, githubUrl }) {
         <a href="https://github.com/zoomKoding/zoomkoding-gatsby-blog">
           &nbsp;zoomkoding-gatsby-blog
         </a>
+        
+        // google adsense
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8568417989764139" crossorigin="anonymous"></script>
+
+        // utterance
+        <script src="https://utteranc.es/client.js"
+            repo="junwork123/junwork123.github.io"
+            issue-term="pathname"
+            label="reply"
+            theme="github-dark"
+            crossorigin="anonymous"
+            async>
+        </script>
       </p>
     </footer>
   );
