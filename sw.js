cd3e248fa84cd7826833d5875b2a24c0e6044c58
junwork123 <junwork123@users.noreply.github.com>
2022-07-02 12:19:02 +0000

    deploy: 8a2113585679984d75c78c96a1552c688515f2ad

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7aee30bac02b91ef64a6.js"
+    "url": "webpack-runtime-9e6e509349b28f889a95.js"
   },
   {
     "url": "styles.853e65090dc89bfbb4ac.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f871a44943ea2cb967cddc5acd5b805b"
+    "revision": "b64b2d7086745e78837cd48ff90f90c1"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
