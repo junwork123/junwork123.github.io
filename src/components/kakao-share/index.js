2696b2d0c173ee9786c7da76ab8184fbc17946e0
MaliciousJ <junwork123@gmail.com>
2022-07-15 23:48:39 +0900

    Update index.js

--- src/components/kakao-share/index.js
+++ src/components/kakao-share/index.js
@@ -2,22 +2,22 @@ import React from "react";
 
 function KakaoShare(params) {
    
-  return;
-  // const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
-	// useEffect(() => {
-	// 	if(status === "ready"){
-  //     // sdk 초기화하기
-  //     Kakao.Share.createCustomButton
-	// 	}
-	// })
-  // Kakao.Share.createCustomButton({
-  //       container: '#create-kakaotalk-sharing-btn',
-  //       templateId: '79194',
-  //       templateArgs: {
-  //         title:'#title'.text(),
-  //         description:'#title'.text(),
-  //       },
-  //     })
+  return (<a></a>);
+  const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	useEffect(() => {
+		if(status === "ready"){
+      // sdk 초기화하기
+      Kakao.Share.createCustomButton
+		}
+	})
+  Kakao.Share.createCustomButton({
+        container: '#create-kakaotalk-sharing-btn',
+        templateId: '79194',
+        templateArgs: {
+          title:'#title'.text(),
+          description:'#title'.text(),
+        },
+      })
     
 }
 
