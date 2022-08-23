a15684594e8e562a29222a2dc8c74e5e949e4dda
MaliciousJ <junwork123@gmail.com>
2022-08-23 19:11:41 +0900

    이름 변경  / about 임시 비활성화

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     ga: '0', // Google Analytics Tracking ID
     author: {
-        name: `최준혁`,
+        name: `주녁`,
         bio: {
             role: `개발자`,
             description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
