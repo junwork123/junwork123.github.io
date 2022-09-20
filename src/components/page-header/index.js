5fb22a49749ce266392312d3ba9264032c74ba64
Junhyeok Choi <junwork123@gmail.com>
2022-09-20 09:29:11 +0900

    Update index.js

--- src/components/page-header/index.js
+++ src/components/page-header/index.js
@@ -48,30 +48,7 @@ function PageHeader({ siteTitle }) {
           {/* 구글 애드센스 설정 */}
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4633249012841159"
             crossorigin="anonymous"></script>
-
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4633249012841159"
-              crossorigin="anonymous"></script>
-          <ins class="adsbygoogle"
-              style="display:block"
-              data-ad-format="autorelaxed"
-              data-ad-client="ca-pub-4633249012841159"
-              data-ad-slot="7705620381"></ins>
-          <script>
-              (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
             
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4633249012841159"
-              crossorigin="anonymous"></script>
-          <ins class="adsbygoogle"
-              style="display:block"
-              data-ad-client="ca-pub-4633249012841159"
-              data-ad-slot="5168469847"
-              data-ad-format="auto"
-              data-full-width-responsive="true"></ins>
-          <script>
-              (adsbygoogle = window.adsbygoogle || []).push({});
-          </script>
-
         </header>
       )}
     />
